bb4d5ab74b0140a3b04162213590eec6a42b403b
chuy-zip <andreschuy03@gmail.com>
2024-05-21 15:29:45 -0600

    responsive fixxes and skills added

--- Portfolio/src/views/Skills.jsx
+++ Portfolio/src/views/Skills.jsx
@@ -1,6 +1,26 @@
-function Skills(){
+function Skills() {
     return (
-        <div id="SkillsSection"> Skills </div>
+        <div id="SkillsSection">
+            <h2 className='secondTitle'></h2>
+            <div className="TwoColumnContainer">
+                <div className="skillsCode">
+                    <h2 className='secondTitle'>Skills</h2>
+                </div>
+                <div className="custom-list-cont">
+                    <ul className="custom-list">
+                        <li>Git and Github</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>Express</li>
+                        <li>C and C++</li>
+                        <li>Java</li>
+                        <li>Kotlin</li>
+                        <li>Docker</li>
+                        <li>Scrum</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
 
