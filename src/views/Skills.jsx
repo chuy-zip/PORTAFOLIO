e506ba49eb4e35d064722d87f93b4a54d4e961fa
chuy-zip <andreschuy03@gmail.com>
2025-12-09 21:27:50 -0600

    updated skilss

--- src/views/Skills.jsx
+++ src/views/Skills.jsx
@@ -1,42 +1,53 @@
 import React from 'react';
-import PercentageCircle from "@components/PercentageCircle";
 
-const skills = [
-    { name: "Git and Github", percentage: '70%' },
-    { name: "JavaScript", percentage: '80%' },
-    { name: "Python", percentage: '80%' },
-    { name: "React", percentage: '80%' },
-    { name: "Express", percentage: '80%' },
-    { name: "C and C++", percentage: '50%' },
-    { name: "Java", percentage: '60%' },
-    { name: "Kotlin", percentage: '60%' },
-    { name: "Docker", percentage: '60%' },
-    { name: "Scrum", percentage: '60%' }
-];
+const skillsByLevel = {
+  Tech: [
+    "Git / GitHub",
+    "JavaScript",
+    "Python",
+    "React",
+    "Express",
+    "Docker",
+    "AWS S3",
+    "Next.js"
+  ],
+  Skills: [
+    "Web Development",
+    "Data Science",
+    "Machine Learning",
+    "Backend Development"
+  ],
+  DB: [
+    "PostgreSQL",
+    "MongoDB",
+  ]
+};
 
 function Skills() {
-    return (
-        <div id="SkillsSection">
-            <h2 className='secondTitle'></h2>
-            <div className="TwoColumnContainer">
-                <div className="skillsCode">
-                    <h2 className='secondTitle'>Skills</h2>
-                </div>
-                <div className="custom-list-cont">
-                    <table className="custom-table">
-                        <tbody>
-                            {skills.map((skill, index) => (
-                                <tr key={index}>
-                                    <td>{skill.name}</td>
-                                    <td><PercentageCircle percentage={skill.percentage} /></td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+  return (
+    <div id="SkillsSection">
+      <h2 className='secondTitle'></h2>
+      <div className="TwoColumnContainer">
+        <div className="skillsCode">
+          <h2 className='secondTitle'>Skills</h2>
         </div>
-    );
+        <div className="custom-list-cont">
+          <div className="skills-grid">
+            {Object.entries(skillsByLevel).map(([level, skills]) => (
+              <div className="skill-category" key={level}>
+                <h3 className="skill-level-title">{level}</h3>
+                <ul className="skill-list">
+                  {skills.map((skill, index) => (
+                    <li key={index}>{skill}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
